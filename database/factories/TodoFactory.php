b3dbdde0ac77234fe1b6ca13fdd96bf78636376a
Thouhedul Islam <tisuchi@gmail.com>
2025-04-14 18:17:34 +0200

    Add migration, model and factories

---
+++ database/factories/TodoFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Todo>
+ */
+class TodoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(),
+            'description' => fake()->paragraph(),
+            'is_completed' => fake()->randomElement([true, false]),
+            'user_id' => User::factory(),
+        ];
+    }
+}
